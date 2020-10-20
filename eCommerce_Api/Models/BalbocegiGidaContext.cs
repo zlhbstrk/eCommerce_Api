ee9b6539f6d94994ead6a23b1e1ad48f52119f29
Zeliha Baştürk <zlhbstrk@hotmail.com>
2020-10-20 15:36:25 +0300

    first commit

---
+++ eCommerce_Api/Models/BalbocegiGidaContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace eCommerce_Api.Models
+{
+    public partial class BalbocegiGidaContext : DbContext
+    {
+        public BalbocegiGidaContext()
+        {
+        }
+
+        public BalbocegiGidaContext(DbContextOptions<BalbocegiGidaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=ZLHBSTRK-PC\\SQLEXPRESS;Database=BalbocegiGida;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Description)
+                    .HasColumnName("description")
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.Image)
+                    .HasColumnName("image")
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.IsStock).HasColumnName("isStock");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Price)
+                    .HasColumnName("price")
+                    .HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.Quantity).HasColumnName("quantity");
+
+                entity.Property(e => e.Status).HasColumnName("status");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ eCommerce_Api/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eCommerce_Api.Models
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+        public string Image { get; set; }
+        public bool? IsStock { get; set; }
+        public int? Quantity { get; set; }
+        public bool? Status { get; set; }
+    }
+}
